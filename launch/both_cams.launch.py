492f64bdf9268fe727076d2dd4428b7a32e9b656
Jaú <jau.gretler@gmail.com>
2026-06-24 15:12:31 +0900

    3chef channel lengths configurable

--- launch/both_cams.launch.py
+++ launch/both_cams.launch.py
@@ -1,7 +1,8 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -21,12 +22,37 @@ def generate_launch_description():
         f"3ch={OPENMV_PARAMS['topic_3_channel']}"
     )
 
+    event_frame_mode_arg = DeclareLaunchArgument(
+        "event_frame_mode",
+        default_value=str(OPENMV_PARAMS.get("event_frame_mode", "cumulative")),
+    )
+    event_frame_ch0_ms_arg = DeclareLaunchArgument(
+        "event_frame_ch0_ms",
+        default_value=str(OPENMV_PARAMS.get("event_frame_ch0_ms", 50.0)),
+    )
+    event_frame_ch1_ms_arg = DeclareLaunchArgument(
+        "event_frame_ch1_ms",
+        default_value=str(OPENMV_PARAMS.get("event_frame_ch1_ms", 250.0)),
+    )
+    event_frame_ch2_ms_arg = DeclareLaunchArgument(
+        "event_frame_ch2_ms",
+        default_value=str(OPENMV_PARAMS.get("event_frame_ch2_ms", 1000.0)),
+    )
+
+    openmv_params = dict(OPENMV_PARAMS)
+    openmv_params.update({
+        "event_frame_mode": LaunchConfiguration("event_frame_mode"),
+        "event_frame_ch0_ms": LaunchConfiguration("event_frame_ch0_ms"),
+        "event_frame_ch1_ms": LaunchConfiguration("event_frame_ch1_ms"),
+        "event_frame_ch2_ms": LaunchConfiguration("event_frame_ch2_ms"),
+    })
+
     openmv_node = Node(
         package='openmv_cam',
         executable='openmv_cam_node',
         name='openmv_cam',
         output='screen',
-        parameters=[OPENMV_PARAMS]
+        parameters=[openmv_params],
     )
 
     realsense_node = IncludeLaunchDescription(
@@ -47,6 +73,10 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        event_frame_mode_arg,
+        event_frame_ch0_ms_arg,
+        event_frame_ch1_ms_arg,
+        event_frame_ch2_ms_arg,
         openmv_node,
-        realsense_node
+        realsense_node,
     ])
